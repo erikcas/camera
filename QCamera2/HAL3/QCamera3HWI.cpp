24cd8e01e6d64cb953b73acdadff0d173c4dd150
Mansoor Aftab <maftab@codeaurora.org>
2016-01-25 20:23:04 +0100

    QCamera2/HAL3: Do not CPP bypass zsl stream when bUseCommonFeatureMask
    If bUseCommonFeatureMask is set, then zsl stream should use
    commonFeatureMask otherwise VFE stream mapping fails
    
    LACAM-1267
    
    Change-Id: Ib7c64979e1ff47219306e978b5b9bbf887df4d0a
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -1271,7 +1271,13 @@ int QCamera3HardwareInterface::configureStreams(
             //for zsl stream the size is jpeg stream size
             mStreamConfigInfo.stream_sizes[i].width = (int32_t)jpegStream->width;
             mStreamConfigInfo.stream_sizes[i].height = (int32_t)jpegStream->height;
-            mStreamConfigInfo.type[i] = CAM_STREAM_TYPE_SNAPSHOT;
+            if (bUseCommonFeatureMask) {
+                mStreamConfigInfo.postprocess_mask[i] =
+                        commonFeatureMask;
+            } else {
+                mStreamConfigInfo.postprocess_mask[i] =
+                        CAM_QCOM_FEATURE_NONE;
+            }
             mStreamConfigInfo.postprocess_mask[i] = CAM_QCOM_FEATURE_NONE;
         } else {
             //for non zsl streams find out the format
